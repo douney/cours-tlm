3b614a493aafd809363136b23ffed4fde75ede38
Félix Robinet <felix.robinet@ensimag.grenoble-inp.fr>
2017-12-01 09:09:50 +0100

    Add files

---
+++ TPs/squelette/tp2/generator.h
@@ -0,0 +1,21 @@
+#ifndef Generator_H
+#define Generator_H
+
+#include "ensitlm.h"
+
+SC_MODULE(Generator) {
+	ensitlm::target_socket<Generator> target;
+
+	SC_HAS_PROCESS(Generator);
+	Generator(sc_core::sc_module_name name, unsigned int size);
+
+	~Generator();
+
+private:
+
+
+public:
+
+};
+
+#endif
