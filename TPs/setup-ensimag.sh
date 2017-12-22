1b49824dac6e5d681ee75fa38306526b4ee3ef8c
fpetrot <frederic.petrot@univ-grenoble-alpes.fr>
2017-12-22 08:18:52 +0100

    Adding Vivado path in setup

--- TPs/setup-ensimag.sh
+++ TPs/setup-ensimag.sh
@@ -3,7 +3,11 @@
 export SYSTEMCROOT=/matieres/5MMMTSP/tlm/systemc-2.3.1/
 
 # Chaine de cross-compilation MicroBlaze, pour le TP3
-xilinx=/matieres/5MMMTSP/tlm/microblaze/setup.sh
+mblaze=/matieres/5MMMTSP/tlm/microblaze/setup.sh
+xilinx=/matieres/5MMMTSP/Xilinx/Vivado/2016.2/settings64.sh
+if [ -f "$mblaze" ]; then
+    source "$mblaze"
+fi
 if [ -f "$xilinx" ]; then
     source "$xilinx"
 fi
